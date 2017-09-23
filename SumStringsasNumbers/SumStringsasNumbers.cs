27e480ed114b2b642807aff7c0dd46a9923093db
Dunk Hsieh Cheng-Lin <dunk.hsieh@titansoft.com.sg>
2017-09-23 16:01:51 +0800

    Modify upload file

---
+++ SumStringsasNumbers/SumStringsasNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumStringsasNumbers
+{
+    class SumStringsasNumbers
+    {
+        public string kata(string str1, string str2)
+        {
+            if (str1 == "" || str2 == "" || str1.Length!=str2.Length)
+                return "000";
+            var groupByString = str1.Select(b => (int)Char.GetNumericValue(b)).Zip(str2.Select(b => (int)Char.GetNumericValue(b)), (x, y) => x + y).ToList();
+            return  String.Concat(groupByString);
+        }
+    }
+}
