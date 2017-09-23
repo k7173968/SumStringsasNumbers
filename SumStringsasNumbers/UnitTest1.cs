f6d8d5e0d88d779732a0e6d20417ac716c1d8d59
Dunk Hsieh Cheng-Lin <dunk.hsieh@titansoft.com.sg>
2017-09-23 15:56:44 +0800

    SumStrinsasNumbers

---
+++ SumStringsasNumbers/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SumStringsasNumbers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var sumStringsasNumbers = new SumStringsasNumbers();
+            Assert.AreEqual("000", sumStringsasNumbers.kata("", "456"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var sumStringsasNumbers = new SumStringsasNumbers();
+            Assert.AreEqual("579", sumStringsasNumbers.kata("123", "456"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var sumStringsasNumbers = new SumStringsasNumbers();
+            Assert.AreEqual("000", sumStringsasNumbers.kata("123", "4567"));
+        }
+    }
+}
